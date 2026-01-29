77369260a663426b23bb92bed3c74cd6419c6af4
iws-seed <iwstechnical7@gmail.com>
2026-01-29 12:23:50 +0100

    done with streaming vercel ai sdk

---
+++ src/2_streaming.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import {streamText} from 'ai';
+import {google} from "@ai-sdk/google";
+
+const model=google("gemini-2.5-flash")
+async function streamingExample() {
+console.log("Streaming response............:\n");
+const result=streamText({
+    model:model,
+    prompt:'write a short poem about Typescript and Ai working together.'
+});
+
+for await (const  chunk of result.textStream) {
+
+// Process each chunk immediately as it arrives
+    // process.stdout.write prints without a newline, creating a smooth flow
+    process.stdout.write(chunk);
+    // add a newline at the end
+    console.log('\n\n---Streaming complete---')
+}
+}
+
+
+streamingExample()
+
