ec7c66ddb08dce56974b67d93fee50c6c706db22
iws-seed <iwstechnical7@gmail.com>
2026-02-04 15:39:44 +0100

    done with the basics of output parsers

---
+++ src/2.0_output_formatting/2.2-basic_structured_output/4_projects/1_project1.ts
@@ -0,0 +1,8 @@
+
+// Form Auto-Fill from Natural Language
+// Imagine a user can describe what they want in natural language, and your app automatically fills in a structured form:
+
+
+import { generateText, Output } from 'ai';
+import { google } from '@ai-sdk/google';
+import { z } from 'zod';
---
+++ src/2.0_output_formatting/2.2-basic_structured_output/4_projects/2_project2.ts
@@ -0,0 +1,9 @@
+
+// Data Extraction from Unstructured Text
+// Another common use case is extracting specific information from documents, emails, or web pages:
+
+
+import { generateText, Output } from 'ai';
+import { google } from '@ai-sdk/google';
+import { z } from 'zod';
+
