34341deaf5eb3d30e0e2c8459334247315c30b76
iws-seed <iwstechnical7@gmail.com>
2026-02-08 23:56:52 +0100

    testing asic embedding functions

--- src/2.1_rags/1_embeddings/3_euclidean_distance.ts
+++ src/2.1_rags/1_embeddings/3_euclidean_distance.ts
@@ -1,1 +1,13 @@
-console.log("Euclidean distance")
+function euclideanDistance(vectorA:number[], vectorB:number[]):number{
+  if(vectorA.length!==vectorB.length){
+    throw new Error('Vectors must have the same length');
+  }
+
+  let sumSquareDifference=0;
+  for(let i=0;i<vectorA.length;i++){
+    const difference=vectorA[i]-vectorB[i];
+    sumSquareDifference+=difference*difference;
+  }
+
+  return Math.sqrt(sumSquareDifference)
+}
